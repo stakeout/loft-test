0cacfd3f3ae080d2c0ce843a488e91d10d47d98e
Vadim Novash <novash@tut.by>
2016-07-26 13:23:28 +0300

    Update 2016-07-26T10:23:11.308Z

--- app/scripts/app.js
+++
@@ -1,9 +0,0 @@
-import svg4everybody from 'svg4everybody';
-import $ from 'jquery';
-import allCoursesChecked from '../blocks/loftblog-courses/loftblog-courses';
-
-$(() => {
-	allCoursesChecked();
-	svg4everybody();
-});
-
