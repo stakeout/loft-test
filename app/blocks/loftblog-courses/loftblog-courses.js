6141bd9e6550c9f6b96a9eb54cf7d4549ff544cb
Vadim Novash <novash@tut.by>
2016-07-26 13:11:55 +0300

    changes after check

--- app/blocks/loftblog-courses/loftblog-courses.js
+++ app/blocks/loftblog-courses/loftblog-courses.js
@@ -1,5 +1,5 @@
 'use strict';
-export default function (){
+export default function () {
 	const coursesVisible = document.querySelector('.loftblog-courses__inner');
 	const chekbox = coursesVisible.querySelectorAll('.checkbox__courses');
 	const ready = coursesVisible.querySelector('.loftblog-courses__all-checked-toggle');
@@ -7,16 +7,17 @@ export default function (){
 	coursesVisible.addEventListener('click', event => {
 		event = event || window.event;
 		const target = event.target || event.srcElement;
+
 		function fadeOut() {
-			target.parentNode.style.visibility = 'hidden';
+			target.parentNode.style.display = 'none';
 		}
-		if (target.type === 'checkbox'){
+		if (target.type === 'checkbox') {
 			if (target.checked) {
 				counter++;
 				target.parentNode.style.backgroundColor = 'rgba(140, 195, 75, .2)';
 				target.nextSibling.nextSibling.style.backgroundColor = 'rgba(140, 195, 75, .2)';
 				setTimeout(fadeOut, 1000);
-				if (counter === chekbox.length){
+				if (counter === chekbox.length) {
 					ready.style.visibility = 'visible';
 					coursesVisible.style.visibility = 'hidden';
 				}
