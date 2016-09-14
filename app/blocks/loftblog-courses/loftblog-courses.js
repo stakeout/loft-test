480916b65f8bde7fb64cabba0910b7d1295bd238
Vadim Novash <novash@tut.by>
2016-09-14 14:05:56 +0300

    fixed bug with js nth-child in courses

--- app/blocks/loftblog-courses/loftblog-courses.js
+++ app/blocks/loftblog-courses/loftblog-courses.js
@@ -1,28 +1,30 @@
 'use strict';
 export default function () {
+	// debugger;
 	const coursesVisible = document.querySelector('.loftblog-courses__inner');
 	const chekbox = coursesVisible.querySelectorAll('.checkbox__courses');
 	const course = coursesVisible.querySelectorAll('.course');
 	const ready = document.querySelector('.loftblog-courses__all-checked-toggle');
 	let counter = 0;
+
 	coursesVisible.addEventListener('click', event => {
 		event = event || window.event;
 		const target = event.target || event.srcElement;
-
 		function fadeOut() {
 			target.parentNode.style.display = 'none';
+
 		}
 		if (target.type === 'checkbox') {
 			if (target.checked) {
 				counter++;
 				target.parentNode.style.backgroundColor = 'rgba(140, 195, 75, .2)';
 				target.nextSibling.nextSibling.style.backgroundColor = 'rgba(140, 195, 75, .2)';
-				setTimeout(fadeOut, 1000);
 				for (let i = 0; i < course.length; i++){
-					if (course[i] % 3 === 0){
-						this.style.marginRight = '0px';
+					if (target.parentNode === course[1]){
+						course[2].style.marginRight = '20px';
 					}
 				}
+				setTimeout(fadeOut, 1000);
 				if (counter === chekbox.length) {
 					ready.style.visibility = 'visible';
 					coursesVisible.style.visibility = 'hidden';
